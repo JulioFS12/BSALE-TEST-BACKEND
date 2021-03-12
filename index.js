09ee4bae303f34e3139657dcaa865c4e649b86b7
Julio Flores <juliocesarflores12@gmail.com>
2021-03-12 14:29:06 -0400

    feat: source public created

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const { port } = require('./config');
+const path = require('path');
+const { static } = require('express');
+
+app.use( '/public' , static(path.join(`${__dirname}/public`)) );
 
 app.listen( port, () => {
     console.log(`Server on port ${port} `);
-} )
+});
