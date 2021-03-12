82ea4063fe8865f84364c76b632445d9254c6045
Julio Flores <juliocesarflores12@gmail.com>
2021-03-12 14:05:58 -0400

    feat: server created

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const environment = {
-    port: process.env.PORT,
+    port: process.env.PORT || 5000,
     user: process.env.USER,
     password: process.env.PASSWORD,
     host: process.env.HOSTING,
