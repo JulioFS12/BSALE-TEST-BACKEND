54a6da5bb52a6b7e4dc0cac5ce6164bd2aea8320
Julio Flores <juliocesarflores12@gmail.com>
2021-03-13 21:38:21 -0400

    feat: queries edited

--- components/product/controller.js
+++ components/product/controller.js
@@ -3,7 +3,7 @@ const pool = require('../../dbConnection');
 
 const getAllProducts = (req, res = response) => {
 
-    pool.query('SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name AS category FROM product AS p INNER JOIN category AS c', function (error, results, fields) {
+    pool.query('SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name AS category FROM product AS p INNER JOIN category AS c WHERE url_image IS NOT NULL', function (error, results, fields) {
         if (error) {
 
             console.log(error);
@@ -23,38 +23,12 @@ const getAllProducts = (req, res = response) => {
 
 }
 
-const getProductByCategory = (req, res = response) => {
-
-    const idCategory = req.query.idCategory;
-
-    pool.query('SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name AS category FROM product AS p INNER JOIN category AS c ON p.category = c.id WHERE p.category = ?', idCategory, function (error, results, fields) {
-
-            if (error) {
-
-                console.log(error);
-    
-                return res.status(400).send({
-                    ok: false,
-                    msg: 'Ups anything was wrong!',
-                });
-            }
-    
-            return res.status(200).send({
-                ok: true,
-                data: results,
-                msg:'Request OK',
-            });
-            
-    });
-
-}
-
 const getProductByName = (req, res = response) => {
 
     const name = req.query.name;
     console.log(name);
 
-    pool.query('SELECT * FROM product WHERE name LIKE ?', `%${name}%` , function (error, results, fields) {
+    pool.query('SELECT * FROM product WHERE name LIKE ? AND url_image IS NOT NULL', `%${name}%` , function (error, results, fields) {
 
             if (error) {
 
@@ -73,6 +47,32 @@ const getProductByName = (req, res = response) => {
             });
             
     });
+}
+
+const getProductByCategory = (req, res = response) => {
+
+    const idCategory = req.query.idCategory;
+
+    pool.query('SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name AS category FROM product AS p INNER JOIN category AS c ON p.category = c.id WHERE p.category = ? AND url_image IS NOT NULL', idCategory, function (error, results, fields) {
+
+            if (error) {
+
+                console.log(error);
+    
+                return res.status(400).send({
+                    ok: false,
+                    msg: 'Ups anything was wrong!',
+                });
+            }
+    
+            return res.status(200).send({
+                ok: true,
+                data: results,
+                msg:'Request OK',
+            });
+            
+    });
+
 }
 
 module.exports = {
